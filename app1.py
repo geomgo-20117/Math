d2a607fa423cbac8c4ab3a7659486b6c53b49eb1
geomgo-20117 <gimpo.s230257@ggh.goe.go.kr>
2024-11-05 16:29:43 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -4,22 +4,22 @@ import streamlit as st
 st.title('동생아 수학 공부하자!')  
 st.subheader('오늘의 주제: 양수와 음수')
 st.write('음수?? 어렵지 않아!') 
-#col1, col2 = st.columns(2)       
-#with col1:
-#      st.subheader('**개념정리')
-#      st.write('- 양수: 0보다 큰 수')
-#      st.write('- 음수: 0보다 작은 수')
-#      st.write('- 양수: (+) / 음수: (-) 부호가 붙음')
-#      st.write('- 부호: (+)는 생략 가능 /(-)는 생략 불가')
-#with col2:
-#      st.image('수학이미지.PNG')
-#a = st.number_input('____________', value= ____)  
+col1, col2 = st.columns(2)       
+with col1:
+      st.subheader('**개념정리')
+      st.write('- 양수: 0보다 큰 수')
+      st.write('- 음수: 0보다 작은 수')
+      st.write('- 양수: (+) / 음수: (-) 부호가 붙음')
+      st.write('- 부호: (+)는 생략 가능 /(-)는 생략 불가')
+with col2:
+      st.image('수학이미지.PNG')
+a = st.number_input('____________', value= ____)  
 
 # 버튼 생성 및 동작
-#if st.button('양수일까 음수일까?'):
-#       if a > 0:
-#              st.write('________')
-#       elif a < 0:
-#              st.write('________')
-#       else:
-#              st.write('________')
+if st.button('양수일까 음수일까?'):
+       if a > 0:
+              st.write('________')
+       elif a < 0:
+              st.write('________')
+       else:
+              st.write('________')
