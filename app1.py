0158c7156224b11bd120f54ff6dde624ad581bc1
geomgo-20117 <gimpo.s230257@ggh.goe.go.kr>
2024-11-05 16:30:23 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -13,7 +13,7 @@ with col1:
       st.write('- 부호: (+)는 생략 가능 /(-)는 생략 불가')
 with col2:
       st.image('수학이미지.PNG')
-a = st.number_input('____________', value= ____)  
+a = st.number_input('수를 입력하세요', value= 0)  
 
 # 버튼 생성 및 동작
 if st.button('양수일까 음수일까?'):
